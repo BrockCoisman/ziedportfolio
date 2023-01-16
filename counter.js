1aa7b6dc7566401a41fec00d8d907eb104a9f9e6
BrockCoisman <91273517+BrockCoisman@users.noreply.github.com>
2023-01-16 13:20:03 -0600

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,30 @@
+function animate(obj, initVal, lastVal, duration) {
+  let startTime = null;
+  let currentTime = Date.now();
+  const step = (currentTime) => {
+    if (!startTime) {
+      startTime = currentTime;
+    }
+
+    const progress = Math.min((currentTime - startTime) / duration, 1);
+
+    obj.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    } else {
+      window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+let text1 = document.getElementById("0101");
+let text2 = document.getElementById("0102");
+let text3 = document.getElementById("0103");
+let text4 = document.getElementById("0104");
+const load = () => {
+  animate(text1, 0, 5, 7000);
+  animate(text2, 0, 5, 7000);
+  animate(text3, 0, 100, 7000);
+  animate(text4, 0, 500, 7000);
+};
---
+++ form.js
@@ -0,0 +1,7 @@
+function formSubmit() {
+    var subject = document.getElementById("form_subject").value;
+    var name = document.getElementById("form_name").value;
+    var uInput = document.getElementById("input_test").value;
+
+    uInput = value;
+}
